834e0246f26c76c6def17d4d4ebf77646b7756a0
TheDeadcoder <sakibbuet2k19@gmail.com>
2024-08-09 18:43:18 +0600

    datetime added to tackle duplication issue

--- app/helpers/supabase_bucket_insert.py
+++ app/helpers/supabase_bucket_insert.py
@@ -1,11 +1,13 @@
 from fastapi import HTTPException
 from app.core.supabase import supabase
 from app.core.config import settings
+from datetime import datetime
 
 bucket_name = settings.SUPABASE_BUCKET_NAME
 def upload_file_to_supabase(file: bytes, filename: str, user_id: str) -> str:
     try:
-        unique_filename = f"{user_id}_{filename}"
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        unique_filename = f"{user_id}_{current_time}_{filename}"
 
         supabase.storage.from_(bucket_name).upload(
             file=file,
